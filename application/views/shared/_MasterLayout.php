d574dbcd8a51926ad2c96de8af699f78b0a4cb44
Luis Najera <lutech1@mac.com>
2015-08-31 11:15:38 -0400

    fixed bug that caused the app not to load.

--- application/views/shared/_MasterLayout.php
+++ application/views/shared/_MasterLayout.php
@@ -12,8 +12,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 
-        <?php require '_StylesPartial.php';?>
-        <?php require '_ScriptsPartial.php';?>
+        <?php require_once '_StylesPartial.php';?>
+        <?php require_once '_ScriptsPartial.php';?>
         
 	    <!-- Mobile Specific Metas -->
 	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -28,7 +28,7 @@
                     <!-- Global Header -->
                     <header id="mainHeader-MenuBar">
                         <div class="header">
-                            <?php require '_HeaderPartial.php'; ?>
+                            <?php require_once '_HeaderPartial.php'; ?>
                         </div>
                     </header>
         
@@ -37,16 +37,16 @@
                         <!-- Main Content Area -->
                         <?php 
 							if (!empty($layout)) {
-								require "application/views/shared/{$layout}.php";
+                                require_once "application/views/shared/{$layout}.php";
 							} else {
-								require "application/views/areas/{$area}/{$page}.php";
+                                require_once "application/views/areas/{$area}/{$page}.php";
 							}
 
 
                             //Common Dialog Modals
-                            require "application/views/shared/_NewMessagePartial.php";
+                        require_once "application/views/shared/_NewMessagePartial.php";
 
-							require '_AdditionalScriptsPartial.php'; 
+                        require_once '_AdditionalScriptsPartial.php';
 							
 							if (!empty($Script)) {
 								echo $Script;
